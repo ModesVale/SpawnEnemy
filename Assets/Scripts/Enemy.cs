5278f2c2a7c4bab827cff3b2c876f7b7a31f1bb6
ModesVale <alex7eremins@gmail.com>
2025-07-17 11:33:11 +0300

    init

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 _moveDirection;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
